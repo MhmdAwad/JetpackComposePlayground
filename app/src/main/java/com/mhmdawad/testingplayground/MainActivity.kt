e23a072f89ec8e32594821f2144b746db32fdc55
mohamed.awad <mhmdawaddd@gmail.com>
2022-08-15 21:34:46 +0200

    played with state

--- app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
+++ app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
@@ -3,21 +3,64 @@ package com.mhmdawad.testingplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.mhmdawad.testingplayground.ui.theme.JetpackComposePlaygroundTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val state = remember {
+                mutableStateOf(Color.Blue)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(state.value)
+                )
+                ColorBox(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize(),
+                    updateColor = {
+                        state.value = it
+                    })
+            }
 
         }
+    }
+
+    @Composable
+    fun ColorBox(
+        modifier: Modifier,
+        updateColor: (Color) -> Unit
+    ) {
+        Box(modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            })
     }
 }
