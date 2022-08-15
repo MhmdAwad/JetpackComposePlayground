53a27d6776ca64736c40bbe09b9d0c0a79a50ae4
mohamed.awad <mhmdawaddd@gmail.com>
2022-08-15 16:21:08 +0200

    columns with modifier and sizing

--- app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
+++ app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
@@ -3,12 +3,15 @@ package com.mhmdawad.testingplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.mhmdawad.testingplayground.ui.theme.JetpackComposePlaygroundTheme
 
@@ -16,28 +19,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+                Text("Hello Awad")
+                Text("important")
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposePlaygroundTheme {
-        Greeting("Android")
     }
 }
