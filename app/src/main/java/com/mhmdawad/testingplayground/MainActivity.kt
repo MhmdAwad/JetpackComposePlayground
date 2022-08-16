c452b3dc76615b5ad125f9a8487b98b9463f1512
mohamed.awad <mhmdawaddd@gmail.com>
2022-08-16 15:32:54 +0200

    played with lazy column

--- app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
+++ app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
@@ -3,21 +3,51 @@ package com.mhmdawad.testingplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mhmdawad.testingplayground.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
+            val list = listOf("1th", "2th", "3th", "4th", "5th", "6th", "7th",
+                "1th", "2th", "3th", "4th", "5th", "6th", "7th",
+                "1th", "2th", "3th", "4th", "5th", "6th", "7th",
+                "1th", "2th", "3th", "4th", "5th", "6th", "7th",
+                "1th", "2th", "3th", "4th", "5th", "6th", "7th")
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                itemsIndexed(
+                    list
+                ) { index, item ->
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 10.dp),
+                        text = "This is the $item!",
+                        textAlign = TextAlign.Center,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            }
         }
+
     }
+
 }
