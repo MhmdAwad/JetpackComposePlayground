4b10d2263dc382e5fa54bd1c39506d7c1fa91169
mohamed.awad <mhmdawaddd@gmail.com>
2022-08-16 15:53:37 +0200

    played with constraint layout with ref id and guideline

--- app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
+++ app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
@@ -3,20 +3,63 @@ package com.mhmdawad.testingplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.mhmdawad.testingplayground.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("green")
+                val redBox = createRefFor("red")
+                val guideline = createGuidelineFromStart(0.5f)
 
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(guideline)
+                    height = Dimension.value(100.dp)
+                    width = Dimension.fillToConstraints
+                }
+
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(guideline)
+                    end.linkTo(parent.end)
+                    height = Dimension.value(100.dp)
+                    width = Dimension.fillToConstraints
+                }
+            }
+
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .layoutId("green")
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .layoutId("red")
+                )
+            }
 
         }
     }
