6d82f0f0d59fb8e0d588f54fcafeb475c9cd12f8
mohamed.awad <mhmdawaddd@gmail.com>
2022-08-16 19:32:28 +0200

    played with animation when change view's size and color

--- app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
+++ app/src/main/java/com/mhmdawad/testingplayground/MainActivity.kt
@@ -3,21 +3,62 @@ package com.mhmdawad.testingplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mhmdawad.testingplayground.ui.theme.JetpackComposePlaygroundTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+            // change size with animations
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+                tween(
+                    durationMillis = 2000,
+                    easing = LinearOutSlowInEasing
+                )
+            )
 
+            //change color with animation
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Blue,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color)
+            ){
+                Button(
+                    modifier = Modifier.align(Alignment.Center),
+                    onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text("Click Me!")
+                }
+            }
         }
     }
 }
